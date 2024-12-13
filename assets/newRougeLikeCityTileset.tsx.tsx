db1e26621904f894fb7555b989e2d271db3f0f33
VolcanicSpectre <sl12doc@gmail.com>
2024-12-13 11:17:40 +0000

    altered maps to be editable
    need to change event timer for demo

---
+++ assets/newRougeLikeCityTileset.tsx.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="newRougeLikeCityTileset.tsx" tilewidth="32" tileheight="32" tilecount="266" columns="19">
+ <image source="roguelikeCity_transparent.png" width="628" height="475"/>
+</tileset>
