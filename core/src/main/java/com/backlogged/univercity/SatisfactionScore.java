7ad3e2778f92d69413daba89934b9d873bd6ae43
Chris <164608901+Chris61912@users.noreply.github.com>
2024-12-18 05:37:40 +0000

    Update SatisfactionScore.java
    Prob will have to tweak the satisfaction score formulas at some point to be a little better but the distance calculations and type works so :)

--- core/src/main/java/com/backlogged/univercity/SatisfactionScore.java
+++ core/src/main/java/com/backlogged/univercity/SatisfactionScore.java
@@ -1,38 +1,200 @@
 package com.backlogged.univercity;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+    
 
 
 public class SatisfactionScore {
+    long score = 0;
+    List<Coord> sceneryCoords = new ArrayList<>();
+    Collection<Building> buildings;
+
+    //simple class to hold coordinates and type of each building
+    
+    private class Pair<T, U> {
+        
+        private Coord coord;
+        private BuildingType type;
+
+        public Pair(Coord coord, BuildingType type) {
+            this.coord = coord;
+            this.type = type;
+        }
+
+        public Coord getMapPos() {
+            return this.coord;
+        }
+        
+        public BuildingType getType() {
+            return this.type;
+        }
+
+        public boolean isboostType(BuildingType type) {
+
+            if (type == null) {
+                return false;
+            }
+            else {
+                return getBoostType() == type;   
+        }
+    }
 
 
-    public double calc_Satisfaction(int distanceClosestBuilding, int distanceBoost, BuildingType type) {
+        public BuildingType getBoostType(){
+            switch (this.type) {
+                case ACCOMMODATION:
+                    return BuildingType.COURSE;
+                case CAFETERIA:
+                    return BuildingType.RECREATIONAL;
+                case COURSE:
+                    return BuildingType.CAFETERIA;
+                case RECREATIONAL:
+                    return null;
+                default:
+                    return null;
+            }
+    }
 
+    
+    }
+
+    public void updateSatisfaction(Collection<Building> buildings) {
+        List<Double> scores = new ArrayList<>();
+        long total = 0;
+        this.buildings = buildings;
+
+
+        
+        for (Pair<Coord,BuildingType> pair : convertToPairList(buildings)) {
+            double test2 = calc_Satisfaction(convertToPairList(buildings), pair);
+            scores.add(test2);
+            total += test2;
+        }
+                
+        score = total;
+
+    }
+
+    //returns the score of a building
+    public long getScore(Building building) {
+        long total = 0;
+        for (Pair<Coord,BuildingType> pair : convertToPairList(building)) {
+            double test2 = calc_Satisfaction(convertToPairList(buildings), pair);
+            total += test2;
+        }
+        return total;
+    }
+
+    public List<Pair<Coord, BuildingType>> convertToPairList(Collection<Building> buildings) {
+        
+        List<Pair<Coord, BuildingType>> pairList = new ArrayList<>();
+
+        for (Building building : buildings) {
+            for (BuildingType type :building.getType()) {                
+                pairList.add(new Pair<>(building.getMapPos(),type));
+            }
+        }
+        return pairList;
+    }
+
+    public List<Pair<Coord, BuildingType>> convertToPairList(Building building) {
+        
+        List<Pair<Coord, BuildingType>> pairList = new ArrayList<>();
+
+            for (BuildingType type :building.getType()) {                
+                pairList.add(new Pair<>(building.getMapPos(),type));
+        }
+        return pairList;
+    }
+    
+    public double calcDistanceClosestBoostBuilding(List<Pair<Coord,BuildingType>> buildings, Pair<Coord,BuildingType> BuildingPair) {
+        double distance = 1000;
+            for (Pair<Coord,BuildingType> building : buildings) {
+                if (!building.getMapPos().equals(BuildingPair.getMapPos()) && BuildingPair.isboostType(building.getType())) {
+                    if (distanceCalc(BuildingPair.getMapPos(), building.getMapPos()) < distance) {
+                        distance = distanceCalc(BuildingPair.getMapPos(), building.getMapPos());
+                    }
+                }
+            }
+            System.out.println("boost...");
+            System.out.println("dis: " + distance);
+            System.out.println("type: " + BuildingPair.getType());
+
+        return Math.round(distance);
+    }
+
+    public double calcDistanceClosestScenery(Pair<Coord,BuildingType> BuildingPair) {
+        double distance = 1000;
+        sceneryCoords.add(new Coord(1, 1));
+        for (Coord scenery : sceneryCoords) {
+            if (distanceCalc(BuildingPair.getMapPos(),scenery) < distance)  {
+                distance = distanceCalc(BuildingPair.getMapPos(),scenery);
+            }
+        }
+        return Math.round(distance);
+    }
+
+    public double calcDistanceClosestBuilding(List<Pair<Coord,BuildingType>> buildings, Pair<Coord,BuildingType> BuildingPair) {
+        double distance = 1000;
+        for (Pair<Coord,BuildingType> building : buildings) {
+            if (!building.getMapPos().equals(BuildingPair.getMapPos()) && !BuildingPair.isboostType(building.getType())) {
+                if (distanceCalc(BuildingPair.getMapPos(), building.getMapPos()) < distance) {
+                    distance = distanceCalc(BuildingPair.getMapPos(), building.getMapPos());
+                }
+            }
+        }
+            System.out.println("building...");
+            System.out.println("dis: " + distance);
+            System.out.println("type: " + BuildingPair.getType());
+        return Math.round(distance);
+
+    }
+
+
+    public double distanceCalc(Coord locA, Coord locB) {        
+        return Math.sqrt(Math.pow(locB.getColumn() - locA.getColumn(), 2) + Math.pow(locB.getRow() - locA.getRow(), 2));
+    }
+
+    public double calc_Satisfaction(List<Pair<Coord,BuildingType>> buildings,Pair<Coord,BuildingType> currentBuilding) {
+        
+        double distanceClosestBoost = 1000;
+        double distanceClosestBuilding = calcDistanceClosestBuilding(buildings, currentBuilding);
+        if (currentBuilding.getType() == BuildingType.RECREATIONAL) {
+            distanceClosestBoost = calcDistanceClosestScenery(currentBuilding);
+           }
+        else {
+            distanceClosestBoost = calcDistanceClosestBoostBuilding(buildings, currentBuilding);
+        }
         if (distanceClosestBuilding > 10) {
             distanceClosestBuilding = 10;
         }
-        if (distanceBoost > 5) {
-            distanceBoost = 5;
+        if (distanceClosestBoost > 10) {
+            distanceClosestBoost = 10;
         }
-
-        double result = (-2.0 / 5.0) * Math.pow(distanceClosestBuilding, 2) + 4 * distanceClosestBuilding + calc_bonus(type, distanceBoost);
+        //does this formula -2/5(x^2) + 4(x) + (bonus)
+        double result = (-2.0 / 5.0) * Math.pow(distanceClosestBuilding, 2) + 4 * distanceClosestBuilding + calc_bonus(currentBuilding.getType(), distanceClosestBoost);
 
         return result;
     }
 
-
-    public int calc_bonus(BuildingType type, int distanceBoost) {
-        int bonusVal = 0;
+    //Calculates bonus for score based on what type of building it is
+    public double calc_bonus(BuildingType type, double distanceBoost) {
+        double bonusVal = 0;
         switch (type) {
             case ACCOMMODATION:
-                bonusVal = (10 - 2 * distanceBoost);
+                bonusVal = (10 - distanceBoost);
                 break;
             case CAFETERIA:
-                bonusVal = (10 - 2 * distanceBoost);
+                bonusVal = (10 - distanceBoost);
                 break;
             case COURSE:
-                bonusVal = (10 - 2 * distanceBoost);
+                bonusVal = (10 - distanceBoost);
                 break;
             case RECREATIONAL:
-                bonusVal = (10 - 2 * distanceBoost);
+                bonusVal = (10 - distanceBoost);
                 break;
             default:
                 bonusVal = 0;
@@ -43,7 +205,6 @@ public class SatisfactionScore {
 
 
     }
-//(-2/5(x^2) + 4(x)) + A(10-2B)
 
 
 }
